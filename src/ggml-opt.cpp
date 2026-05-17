5fc38d27b3e8754cef405a910b77077b0411f174
Johannes Gäßler <johannesg@5d6.de>
2026-05-17 20:45:12 +0200

    mean rel error outputs

--- src/ggml-opt.cpp
+++ src/ggml-opt.cpp
@@ -446,6 +446,34 @@ static void ggml_opt_build(ggml_opt_context_t opt_ctx) {
             opt_ctx->loss_per_datapoint = true;
             break;
         }
+        case GGML_OPT_LOSS_TYPE_MEAN_SQUARED_ERROR_REL_OUTPUTS: {
+            opt_ctx->labels = ggml_dup_tensor(ctx_results, opt_ctx->outputs);
+            ggml_set_input(opt_ctx->labels);
+            ggml_set_name(opt_ctx->labels, "labels");
+            opt_ctx->loss = ggml_sub(ctx_results, opt_ctx->outputs, opt_ctx->labels);
+            ggml_set_name(opt_ctx->loss, "loss_error");
+            opt_ctx->loss = ggml_div(ctx_results, opt_ctx->loss, opt_ctx->outputs);
+            ggml_set_name(opt_ctx->loss, "loss_error_rel_outputs");
+            opt_ctx->loss = ggml_sqr(ctx_results, opt_ctx->loss);
+            ggml_set_name(opt_ctx->loss, "loss_squared_error_rel_outputs");
+            opt_ctx->loss = ggml_sum(ctx_results, opt_ctx->loss);
+            ggml_set_name(opt_ctx->loss, "loss_sum_squared_error_rel_outputs");
+
+            ggml_tensor * std_penalty = ggml_sqr(ctx_results, opt_ctx->outputs);
+            ggml_set_name(std_penalty, "loss_var");
+            std_penalty = ggml_log(ctx_results, std_penalty);
+            ggml_set_name(std_penalty, "loss_log_var");
+            std_penalty = ggml_sum(ctx_results, std_penalty);
+            ggml_set_name(std_penalty, "loss_sum_log_var");
+            opt_ctx->loss = ggml_add(ctx_results, opt_ctx->loss, std_penalty);
+            ggml_set_name(opt_ctx->loss, "loss_sum_squared_error_log_var");
+
+            const float scale = 1.0f / (opt_ctx->opt_period * ggml_nelements(opt_ctx->outputs));
+            opt_ctx->loss = ggml_scale(ctx_results, opt_ctx->loss, scale);
+            ggml_set_name(opt_ctx->loss, "loss_mean_squared_error_rel_outputs");
+            opt_ctx->loss_per_datapoint = true;
+            break;
+        }
         case GGML_OPT_LOSS_TYPE_MEAN_SQUARED_ASYMMETRY: {
             opt_ctx->labels = ggml_dup_tensor(ctx_results, opt_ctx->outputs);
             ggml_set_input(opt_ctx->labels);
